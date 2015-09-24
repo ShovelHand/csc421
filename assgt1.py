0cedc7ebe853f8e14ae2d080e4aa07648461a8a6
Alex Carmichael <carmichael.alexander@gmail.com>
2015-09-24 12:26:21 -0700

    now finding nearest neighbours for each city

--- assgt1.py
+++ assgt1.py
@@ -25,6 +25,7 @@ for i in alphabet:
             grid[x][y] = i
             placed = True
  #uncomment to print the grid of letters
+     '''       
 for j in range(0,100):
     rowOut = ""
     for i in range(0,100):
@@ -33,11 +34,25 @@ for j in range(0,100):
      else:
          rowOut += grid[i][j]
         
-    print(rowOut )  
+    print(rowOut )
+'''    
 
 def euclidDist(x1,y1,x2,y2):
     return math.sqrt((x1-x2)**2 +(y1-y2)**2)
 
+def getFiveNearest(lst, i ,j):
+    nearestFive = []
+    for city in lst:
+        for y in range(100):
+            for x in range(100):
+                if(grid[x][y] == city):
+                    dist = euclidDist(i,j,x,y)
+                    nearestFive.append((city, dist))
+    nearestFive.sort(key = lambda x : x[1])
+    print(nearestFive[:5])
+    return nearestFive[:5]
+                    
+    
 #this function doesn't work properly and should get fixed    
 def findNeighbours(c, i, j, lst, depth):
     addRight = 0; #when increasing the search bounds and encountering an array edge,
@@ -45,51 +60,43 @@ def findNeighbours(c, i, j, lst, depth):
 
     leftCol = i - depth
     if(leftCol < 0):
-       # addRight =  abs((i - depth))
+        addRight =  abs((i - depth))
         leftCol = 0
 
-    rightCol = i + depth + addRight
+    rightCol = i + depth
     if(rightCol > 100):
-        leftCol -= (depth)
         rightCol = 100
 
     topRow = j - depth 
     if(topRow < 0):
-      #  addBottom = abs(j - depth)
+        addBottom = abs(j - depth)
         topRow = 0
    
-    bottomRow = j + depth + addBottom
+    bottomRow = j + depth
     if(bottomRow > 100):
-        topRow -= ( depth)
         bottomRow = 100
-
-
         
-    for n in range(topRow, bottomRow):
-        for m in range(leftCol, rightCol):
+    for n in range(100):
+        for m in range(100):
             
             if(grid[m][n] != '0' and (grid[m][n] != c) and ((grid[m][n])) not in lst):
                 lst.append(grid[m][n]) 
-    if(len(lst) >= 5):
-        print (c)
-        print  (lst)
-        if( c == 'l'):
-            print(i,j)
-        return lst
+
+    if(len(lst) >= 10):
+      #  print (c)
+        return getFiveNearest(lst, i, j)
     else:
         if(depth < 100):
             findNeighbours(c, i, j, lst, depth+1)
-
             
 #cities placed in grid. Now determine adjacencies
 for j in range(0,100):
     for i in range(0,100):
         if(grid[i][j] != '0'):
             neighbourList = []
+            print(grid[i][j])
             neighbourList = findNeighbours(grid[i][j], i, j, neighbourList, 1)
-           
-       
-           # neighbourList = []
+            
 
 #print(adjMatrix)
       
